be85004bb73da3ac6cc13c31d5464e628672051f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-03-11 00:20:20 +0900

    first commit

---
+++ src/main/java/net/blogteamthreecoderhivebe/BlogTeamThreeCoderHiveBeApplication.java
@@ -0,0 +1,13 @@
+package net.blogteamthreecoderhivebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogTeamThreeCoderHiveBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogTeamThreeCoderHiveBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/blogteamthreecoderhivebe/BlogTeamThreeCoderHiveBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.blogteamthreecoderhivebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogTeamThreeCoderHiveBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
