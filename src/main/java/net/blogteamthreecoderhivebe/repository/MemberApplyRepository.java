b2b93be697c36ca0ac986c616998ac3fbf590ea7
hxnsxxm <hansaemj20@gmail.com>
2023-03-24 00:02:58 +0900

    refactor: 모든 repository 초기 세팅
    Fixes: #20

---
+++ src/main/java/net/blogteamthreecoderhivebe/repository/MemberApplyRepository.java
@@ -0,0 +1,8 @@
+package net.blogteamthreecoderhivebe.repository;
+
+import net.blogteamthreecoderhivebe.entity.MemberApply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberApplyRepository extends JpaRepository<MemberApply, Long> {
+
+}
--- src/main/java/net/blogteamthreecoderhivebe/repository/PostRepository.java
+++ src/main/java/net/blogteamthreecoderhivebe/repository/PostRepository.java
@@ -1,7 +1,6 @@
 package net.blogteamthreecoderhivebe.repository;
 
 import net.blogteamthreecoderhivebe.entity.Post;
-import net.blogteamthreecoderhivebe.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
