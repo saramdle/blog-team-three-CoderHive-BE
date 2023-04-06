69f674d08171e37989acbb8c3543d5e9655e9e0e
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-04-06 17:26:19 +0900

    feat: 모든 location 정보 가져오기
    컨트롤러 서비스 를 구현하고 레포지토리는 spring data jpa 기능 사용
    응답으로 LocationsResponse 를 이용해 json 형태로 감싸 보냄

---
+++ src/main/java/net/blogteamthreecoderhivebe/controller/TableInfoController.java
@@ -0,0 +1,21 @@
+package net.blogteamthreecoderhivebe.controller;
+
+import lombok.RequiredArgsConstructor;
+import net.blogteamthreecoderhivebe.dto.response.LocationsResponse;
+import net.blogteamthreecoderhivebe.service.TableInfoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RequestMapping("info")
+@RestController
+public class TableInfoController {
+    private final TableInfoService tableInfoService;
+
+    @GetMapping("/locations")
+    public LocationsResponse searchAllLocation() {
+        return LocationsResponse.from(tableInfoService.searchAllLocation());
+    }
+}
