d75e495355a99545797c72fab522334c9636aefa
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-04-07 15:14:23 +0900

    feat: 모든 직무 불러오기 구현
    레포지토리에서 entity로 불러와서
    서비스에서 요구조건에 맞게 변경

--- src/main/java/net/blogteamthreecoderhivebe/controller/TableInfoController.java
+++ src/main/java/net/blogteamthreecoderhivebe/controller/TableInfoController.java
@@ -1,6 +1,7 @@
 package net.blogteamthreecoderhivebe.controller;
 
 import lombok.RequiredArgsConstructor;
+import net.blogteamthreecoderhivebe.dto.response.JobsResponse;
 import net.blogteamthreecoderhivebe.dto.response.LocationsResponse;
 import net.blogteamthreecoderhivebe.service.TableInfoService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,5 +18,10 @@ public class TableInfoController {
     @GetMapping("/locations")
     public LocationsResponse searchAllLocation() {
         return LocationsResponse.from(tableInfoService.searchAllLocation());
+    }
+
+    @GetMapping("/jobs")
+    public JobsResponse searchAllJobs() {
+        return JobsResponse.from(tableInfoService.searchAllJobs());
     }
 }
