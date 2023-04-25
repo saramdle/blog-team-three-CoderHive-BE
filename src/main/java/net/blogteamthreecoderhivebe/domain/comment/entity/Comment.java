f4ca28bac0a983c48a125864c2476c7abbbc407e
anneu <b.imnuj@gmail.com>
2023-04-25 16:25:02 +0900

    Refactor: 댓글을 Reply에서 Comment로 변경

--- src/main/java/net/blogteamthreecoderhivebe/domain/comment/entity/Comment.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/comment/entity/Comment.java
@@ -9,13 +9,13 @@ import net.blogteamthreecoderhivebe.global.common.AuditingFields;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
-@ToString(callSuper = true, exclude = {"member", "post", "childReplys"})
+@ToString(callSuper = true, exclude = {"member", "post", "childComments"})
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Reply extends AuditingFields {
+public class Comment extends AuditingFields {
     @Id
-    @Column(name = "reply_id")
+    @Column(name = "comment_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -31,18 +31,19 @@ public class Reply extends AuditingFields {
     private String content;
 
     @OrderBy("createdAt ASC") // 대댓글은 먼저 생성된 것부터 순차적으로 정렬
-    @OneToMany(mappedBy = "parentReplyId", cascade = CascadeType.ALL) //부모가 지워지면 자식이 전부 지워지게
-    private Set<Reply> childReplys = new LinkedHashSet<>(); //jpa 에서는 final 을 추천하지 않는다.
+    @OneToMany(mappedBy = "parentCommentId", cascade = CascadeType.ALL) //부모가 지워지면 자식이 전부 지워지게
+    private Set<Comment> childComments = new LinkedHashSet<>(); //jpa 에서는 final 을 추천하지 않는다.
 
     @Setter
     @Column(updatable = false)
-    private Long parentReplyId; //부모 댓글 ID ArticleComment parentComment 이런식으로 하면 양방향이 가능, 여기서는 단방향으로 설정
+    private Long parentCommentId; //부모 댓글 ID ArticleComment parentComment 이런식으로 하면 양방향이 가능, 여기서는 단방향으로 설정
 
     @Builder
-    public Reply(Long id, Member member, Post post, Long parentReplyId) {
+    public Comment(Long id, Member member, Post post, String content, Long parentCommentId) {
         this.id = id;
         this.member = member;
         this.post = post;
-        this.parentReplyId = parentReplyId;
+        this.content = content;
+        this.parentCommentId = parentCommentId;
     }
 }
--- src/main/java/net/blogteamthreecoderhivebe/domain/comment/repository/ReplyRepository.java
+++
@@ -1,8 +0,0 @@
-package net.blogteamthreecoderhivebe.domain.comment.repository;
-
-import net.blogteamthreecoderhivebe.domain.comment.entity.Reply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReplyRepository extends JpaRepository<Reply, Long> {
-
-}
