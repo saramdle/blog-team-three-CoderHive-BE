9c3a6cb42afc5eb72106c0e9065bea73b1f8d19a
anneu <b.imnuj@gmail.com>
2023-04-27 04:10:55 +0900

    Refactor: 소셜 로그인 서비스가 무엇인지에 따라 다른 생성자를 호출하는 역할을 SocialLoginDto 객체에게 위임

--- src/main/java/net/blogteamthreecoderhivebe/global/auth/dto/SocialLoginDto.java
+++ src/main/java/net/blogteamthreecoderhivebe/global/auth/dto/SocialLoginDto.java
@@ -2,9 +2,21 @@ package net.blogteamthreecoderhivebe.global.auth.dto;
 
 import lombok.Builder;
 
+import java.util.Map;
+
 @Builder
 public record SocialLoginDto( // String nickname,
                               String email) {
+    public static SocialLoginDto of(String registrationId, Map<String, Object> attributes) {
+        if (registrationId.toUpperCase().equals("KAKAO")) {
+            return fromKakao(KakaoOAuth2Response.from(attributes));
+        }
+        if (registrationId.toUpperCase().equals("NAVER")) {
+            return fromNaver(NaverOAuth2Response.from(attributes));
+        }
+        return fromGoogle(GoogleOAuth2Response.from(attributes));
+    }
+
     public static SocialLoginDto fromKakao(KakaoOAuth2Response kakaoResponse) {
         return SocialLoginDto.builder()
                 //.nickname(kakaoResponse.nickname())
