e14137303db30196aad96fedb9031cf109025799
hansaem <hansaemj20@gmail.com>
2023-04-29 03:46:59 +0900

    Refactor: Service단에 SignUpDto를 씌워서 전달하고, SignUpResponse를 받아 반환하도록 수정

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/controller/MemberController.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/controller/MemberController.java
@@ -3,17 +3,16 @@ package net.blogteamthreecoderhivebe.domain.member.controller;
 import lombok.RequiredArgsConstructor;
 import net.blogteamthreecoderhivebe.domain.heart.service.HeartService;
 import net.blogteamthreecoderhivebe.domain.member.dto.MemberWithPostDto;
+import net.blogteamthreecoderhivebe.domain.member.dto.SignUpDto;
 import net.blogteamthreecoderhivebe.domain.member.dto.request.SignUpRequest;
 import net.blogteamthreecoderhivebe.domain.member.dto.response.MemberInfoWithPostResponse;
 import net.blogteamthreecoderhivebe.domain.member.dto.response.MyInfoWithPostResponse;
+import net.blogteamthreecoderhivebe.domain.member.dto.response.SignUpResponse;
 import net.blogteamthreecoderhivebe.domain.member.service.MemberService;
 import net.blogteamthreecoderhivebe.global.auth.dto.MemberPrincipal;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,18 +22,36 @@ public class MemberController {
     private final MemberService memberService;
     private final HeartService heartService;
 
+    /**
+     * 회원 가입 - 추가 정보 등록
+     * need   > SignUpRequest, MemberPrincipal
+     * return > SignInResponse
+     * TODO 1) Dto 생성 / SignUpRequest, SignUpDto, SignInResponse
+     * TODO 2) 사용자 정보 유효성 검사 / email, nickname, jobId, career, level
+     * TODO 3) 사용자 정보 저장
+     */
     @PostMapping
-    public ResponseEntity<String> signUp(@RequestBody SignUpRequest signUpRequest,
-                                         @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
-        Long memberId = memberService.save(signUpRequest, memberPrincipal.getEmail());
+    //public SingUpResponse signUp(@RequestBody @Valid SignUpRequest signUpRequest,
+    public SignUpResponse signUp(@RequestBody SignUpRequest signUpRequest,
+                                 @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
+//        Long memberId = memberService.signUp(SignUpDto.of(signUpRequest, memberPrincipal.getEmail()));
+        SignUpResponse signUpResponse = memberService.signUp(SignUpDto.of(signUpRequest, memberPrincipal.getEmail()));
 
-        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(memberId)
-                .toUri();
-
-        return ResponseEntity.created(location).body("회원 가입 성공");
+//        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+//                .path("/{id}")
+//                .buildAndExpand(memberId)
+//                .toUri();
+        return signUpResponse;
+        //return ResponseEntity.created(location).body("회원 가입 성공");
     }
+
+    /**
+     * 유효한 회원인지 확인 기능
+     * need   > email
+     * return > SignUpResponse
+     * TODO 1)
+     */
+
 
     @GetMapping("/my")
     public MyInfoWithPostResponse searchMyInfo(@RequestParam Long memberId) {
