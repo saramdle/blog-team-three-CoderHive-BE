36e28c5bd140cb7c211e2d3b3713ed08298c9c09
anneu <b.imnuj@gmail.com>
2023-05-08 16:18:20 +0900

    Feat: 카테고리 조회 기능 구현

--- src/main/java/net/blogteamthreecoderhivebe/domain/post/constant/PostCategory.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/post/constant/PostCategory.java
@@ -2,18 +2,26 @@ package net.blogteamthreecoderhivebe.domain.post.constant;
 
 import lombok.Getter;
 
+import java.util.Arrays;
+
 @Getter
 public enum PostCategory {
     PROJECT("project"),
     STUDY("study");
 
-    @Getter
-    private final String description;
+    private static final String NOT_FOUND_CATEGORY = "[%s] 카테고리를 찾을 수 없습니다.";
 
     private final String key;
 
     PostCategory(String key) {
         this.key = key;
+    }
+
+    public static PostCategory find(String key) {
+        return Arrays.stream(values())
+                .filter(category -> category.key.equals(key))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format(NOT_FOUND_CATEGORY, key)));
     }
 }
 
