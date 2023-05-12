35c665e6c400d1e3f1d8414ae2707eb91b54d6a9
hansaem <hansaemj20@gmail.com>
2023-05-12 15:49:52 +0900

    Refactor: 어노테이션 NotNull에서 NotBlack로 변경

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/request/SignUpRequest.java
@@ -1,14 +1,13 @@
 package net.blogteamthreecoderhivebe.domain.member.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
 public record SignUpRequest(
         @NotBlank(message = "닉네임은 필수 입력값입니다.")
         @Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,10}$", message = "닉네임은 특수문자를 포함하지 않은 2~10자리여야 합니다.")
         String nickname,
-        @NotNull(message = "직무는 필수 입력값입니다.")
+        @NotBlank(message = "직무는 필수 입력값입니다.")
         @Pattern(regexp = "^[0-9]{1,10}$", message = "직무는 1 이상의 1~10자리 숫자여야 합니다.")
         String jobId,
         @NotBlank(message = "레벨은 필수 입력값입니다.")
