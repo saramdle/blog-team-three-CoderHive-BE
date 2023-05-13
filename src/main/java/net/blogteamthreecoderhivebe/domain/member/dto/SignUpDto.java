1d57fec89e25f735d133ff31934389cd0f53d30d
hansaem <hansaemj20@gmail.com>
2023-05-13 15:19:05 +0900

    Style: 불필요한 주석 및 코드 제거

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/SignUpDto.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/SignUpDto.java
@@ -1,13 +1,9 @@
 package net.blogteamthreecoderhivebe.domain.member.dto;
 
 import lombok.Builder;
-import net.blogteamthreecoderhivebe.domain.info.entity.Job;
 import net.blogteamthreecoderhivebe.domain.member.constant.MemberCareer;
 import net.blogteamthreecoderhivebe.domain.member.constant.MemberLevel;
 import net.blogteamthreecoderhivebe.domain.member.dto.request.SignUpRequest;
-import net.blogteamthreecoderhivebe.domain.member.entity.Member;
-
-import static net.blogteamthreecoderhivebe.domain.member.constant.MemberRole.USER;
 
 @Builder
 public record SignUpDto(
@@ -24,17 +20,6 @@ public record SignUpDto(
                 .jobId(Long.parseLong(signUpRequest.jobId()))
                 .memberLevel(MemberLevel.find(signUpRequest.level()))
                 .memberCareer(MemberCareer.find(signUpRequest.career()))
-                .build();
-    }
-
-    public static Member toMemberWithJob(SignUpDto dto, Job job) {
-        return Member.builder()
-                .email(dto.email)
-                .nickname(dto.nickname)
-                .career(dto.memberCareer)
-                .job(job)
-                .level(dto.memberLevel)
-                .memberRole(USER)
                 .build();
     }
 }
