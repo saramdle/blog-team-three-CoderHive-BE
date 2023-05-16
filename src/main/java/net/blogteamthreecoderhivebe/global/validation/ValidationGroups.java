2412f919641738d03d86dbe34df22e36c6b898fa
hansaem <hansaemj20@gmail.com>
2023-05-16 17:52:35 +0900

    Feat: 입력값 검증(Validation)시 @NotBlank와 @Pattern을 구분하여 검증하기 위한 클래스/인터페이스 추가

---
+++ src/main/java/net/blogteamthreecoderhivebe/global/validation/ValidationGroups.java
@@ -0,0 +1,13 @@
+package net.blogteamthreecoderhivebe.global.validation;
+
+public class ValidationGroups {
+    public interface NotEmptyGroup {
+    }
+
+    ;
+
+    public interface PatternCheckGroup {
+    }
+
+    ;
+}
