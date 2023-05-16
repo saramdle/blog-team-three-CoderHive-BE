213c4c128b47f022ff9ad98347c1e6b1762d71f4
anneu <b.imnuj@gmail.com>
2023-05-16 18:08:30 +0900

    Refactor: 회원이 좋아요한 게시글을 조회하는 기능 개선
    - 조회 관련 쿼리는 querydsl을 사용하도록 분리
    - 회원 id를 통해 회원이 좋아요한 게시글의 목록을 조회하는 기능 구현

--- src/main/java/net/blogteamthreecoderhivebe/domain/heart/service/HeartService.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/heart/service/HeartService.java
@@ -2,20 +2,21 @@ package net.blogteamthreecoderhivebe.domain.heart.service;
 
 import lombok.RequiredArgsConstructor;
 import net.blogteamthreecoderhivebe.domain.heart.repository.HeartRepository;
+import net.blogteamthreecoderhivebe.domain.post.entity.Post;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@Transactional
 @Service
 public class HeartService {
     private final HeartRepository heartRepository;
 
+    @Transactional(readOnly = true)
     public List<Long> searchHeartPostIds(Long memberId) {
-        return heartRepository.findHeartByMember_Id(memberId).stream()
-                .map(p -> p.getPost().getId())
+        return heartRepository.findHeartPosts(memberId).stream()
+                .map(Post::getId)
                 .toList();
     }
 }
