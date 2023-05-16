529bca36d29c1080d469b976f69ab37fc8f34c2c
hansaem <hansaemj20@gmail.com>
2023-05-16 18:00:29 +0900

    Style: 필드간 개행 추가

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/request/SignUpRequest.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/request/SignUpRequest.java
@@ -11,16 +11,19 @@ public record SignUpRequest(
                 message = "닉네임은 특수문자를 포함하지 않은 2~10자리여야 합니다.",
                 groups = ValidationGroups.PatternCheckGroup.class)
         String nickname,
+
         @NotBlank(message = "직무는 필수 입력값입니다.",
                 groups = ValidationGroups.NotEmptyGroup.class)
         @Pattern(regexp = "^[0-9]{1,10}$", message = "직무는 1 이상의 1~10자리 숫자여야 합니다.",
                 groups = ValidationGroups.PatternCheckGroup.class)
         String jobId,
+
         @NotBlank(message = "레벨은 필수 입력값입니다.",
                 groups = ValidationGroups.NotEmptyGroup.class)
         @Pattern(regexp = "^[가-힣a-zA-Z0-9-_~]{1,20}$", message = "레벨은 정해진 description을 만족해야 합니다.",
                 groups = ValidationGroups.PatternCheckGroup.class)
         String level,
+
         @NotBlank(message = "경력은 필수 입력값입니다.",
                 groups = ValidationGroups.NotEmptyGroup.class)
         @Pattern(regexp = "^[가-힣a-zA-Z0-9-_~]{1,20}$", message = "경력은 정해진 description을 만족해야 합니다.",
