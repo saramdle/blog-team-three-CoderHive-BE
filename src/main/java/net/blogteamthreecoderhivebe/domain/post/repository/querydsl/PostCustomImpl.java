afaa30459a4f85158de2dc8bf8d8ab29232b0851
anneu <b.imnuj@gmail.com>
2023-06-01 15:58:48 +0900

    Comment: 주석 추가

--- src/main/java/net/blogteamthreecoderhivebe/domain/post/repository/querydsl/PostCustomImpl.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/post/repository/querydsl/PostCustomImpl.java
@@ -67,9 +67,9 @@ public class PostCustomImpl implements PostCustom {
                         eqPostCategory(category), inLocations(locations), inJobs(jobs), eqPostStatus(status)
                 )
                 .groupBy(post.id)
-                .offset(pageable.getOffset())
-                .limit(pageable.getPageSize())
-                .orderBy(post.modifiedAt.desc())
+                .offset(pageable.getOffset()) // 어디서부터 보여줄 건지
+                .limit(pageable.getPageSize()) // 한 페이지에 보여줄 개수
+                .orderBy(post.modifiedAt.desc()) // 가장 최근 글이 맨 앞에 오도록 정렬
                 .fetch();
 
         JPAQuery<Long> countQuery = queryFactory
@@ -79,18 +79,30 @@ public class PostCustomImpl implements PostCustom {
         return PageableExecutionUtils.getPage(posts, pageable, countQuery::fetchOne);
     }
 
+    /**
+     * 카테고리 검색 조건
+     */
     private BooleanExpression eqPostCategory(PostCategory postCategory) {
         return postCategory != null ? post.postCategory.eq(postCategory): null;
     }
 
+    /**
+     * 모집상태 검색 조건
+     */
     private BooleanExpression eqPostStatus(PostStatus postStatus) {
         return postStatus != null ? post.postStatus.eq(postStatus) : null;
     }
 
+    /**
+     * 지역 검색 조건
+     */
     private BooleanExpression inLocations(List<Long> locations) {
         return locations != null ? post.location.id.in(locations) : null;
     }
 
+    /**
+     * 직무 검색 조건
+     */
     private BooleanExpression inJobs(List<Long> jobs) {
         return jobs != null ? recruitmentJob.job.id.in(jobs) : null;
     }
