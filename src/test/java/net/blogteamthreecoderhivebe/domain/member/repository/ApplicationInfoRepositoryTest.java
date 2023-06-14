0700903fd6a270946c4002282bb6bb68a6f1e3be
anneu <b.imnuj@gmail.com>
2023-06-14 23:40:57 +0900

    Test: 테스트에 public 및 @Transactional 제거
    - @DataJpaTest에 @Transactinal이 포함되어 있으므로 제거

--- src/test/java/net/blogteamthreecoderhivebe/domain/member/repository/ApplicationInfoRepositoryTest.java
+++ src/test/java/net/blogteamthreecoderhivebe/domain/member/repository/ApplicationInfoRepositoryTest.java
@@ -8,15 +8,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @DisplayName("ApplicationInfoRepository 테스트")
 @Import({TestJpaConfig.class, TestQueryDslConfig.class})
-@Transactional
 @DataJpaTest
-public class ApplicationInfoRepositoryTest {
+class ApplicationInfoRepositoryTest {
     @Autowired
     private ApplicationInfoRepository applicationInfoRepository;
 
