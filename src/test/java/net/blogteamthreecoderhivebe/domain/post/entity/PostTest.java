0a886e55df888b0ff2f64aed352f70ffa120a50b
anneu <b.imnuj@gmail.com>
2023-06-15 01:44:28 +0900

    Test: 게시글의 플랫폼 목록을 List로 변환하여 가져오는 기능 테스트 추가

--- src/test/java/net/blogteamthreecoderhivebe/domain/post/entity/PostTest.java
+++ src/test/java/net/blogteamthreecoderhivebe/domain/post/entity/PostTest.java
@@ -2,6 +2,8 @@ package net.blogteamthreecoderhivebe.domain.post.entity;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PostTest {
@@ -10,5 +12,19 @@ class PostTest {
         Post post = Post.builder().build();
         assertThat(post.getHearts()).isNotNull();
         assertThat(post.getRecruitmentJobs()).isNotNull();
+    }
+
+    @Test
+    void getPlatformList() {
+        Post post1 = Post.builder().platforms("플랫폼1").build();
+        Post post2 = Post.builder().platforms("플랫폼1&플랫폼2").build();
+
+        List<String> platformList1 = post1.getPlatformList();
+        List<String> platformList2 = post2.getPlatformList();
+
+        assertThat(platformList1).hasSize(1);
+        assertThat(platformList2).hasSize(2);
+        assertThat(platformList1).containsExactly("플랫폼1");
+        assertThat(platformList2).containsExactly("플랫폼1","플랫폼2");
     }
 }
