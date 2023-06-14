9c965bb518a70a0d888ed0b910a4c6aaf82844e3
anneu <b.imnuj@gmail.com>
2023-06-14 23:32:04 +0900

    Comment: 주석 추가

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/service/ApplicationInfoService.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/service/ApplicationInfoService.java
@@ -16,12 +16,19 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 @Service
 public class ApplicationInfoService {
+
     private final ApplicationInfoRepository applicationInfoRepository;
 
+    /**
+     * 회원의 지원 정보 조회
+     */
     public List<ApplicationInfo> searchApplyPost(Long memberId) {
         return applicationInfoRepository.findByMember_Id(memberId);
     }
 
+    /**
+     * 회원이 모집직무에 지원한 결과 조회
+     */
     public ApplicationResult getApplyResult(Member member, RecruitmentJob recruitmentJob) {
         Optional<ApplicationResult> applyResult = applicationInfoRepository.findApplyResult(member, recruitmentJob);
         // 지원 이력이 없을 경우 미신청
