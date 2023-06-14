0c211fe322ff36502eed3767c37443c112cdc98a
anneu <b.imnuj@gmail.com>
2023-06-14 23:43:05 +0900

    Test: 회원이 모집중인 직무에 지원한 결과를 조회하는 기능 테스트 추가

--- src/test/java/net/blogteamthreecoderhivebe/domain/member/repository/ApplicationInfoRepositoryTest.java
+++ src/test/java/net/blogteamthreecoderhivebe/domain/member/repository/ApplicationInfoRepositoryTest.java
@@ -1,8 +1,13 @@
 package net.blogteamthreecoderhivebe.domain.member.repository;
 
+import net.blogteamthreecoderhivebe.domain.member.constant.ApplicationResult;
 import net.blogteamthreecoderhivebe.domain.member.entity.ApplicationInfo;
+import net.blogteamthreecoderhivebe.domain.member.entity.Member;
+import net.blogteamthreecoderhivebe.domain.post.entity.RecruitmentJob;
+import net.blogteamthreecoderhivebe.domain.post.repository.RecruitmentJobRepository;
 import net.blogteamthreecoderhivebe.global.config.TestJpaConfig;
 import net.blogteamthreecoderhivebe.global.config.TestQueryDslConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +15,31 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import java.util.List;
+import java.util.Optional;
 
-@DisplayName("ApplicationInfoRepository 테스트")
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Import({TestJpaConfig.class, TestQueryDslConfig.class})
 @DataJpaTest
 class ApplicationInfoRepositoryTest {
-    @Autowired
-    private ApplicationInfoRepository applicationInfoRepository;
+
+    @Autowired ApplicationInfoRepository applicationInfoRepository;
+    @Autowired MemberRepository memberRepository;
+    @Autowired RecruitmentJobRepository recruitmentJobRepository;
+
+    Member member;
+    RecruitmentJob recruitmentJob;
+
+    @BeforeEach
+    void setUp() {
+        member = memberRepository.save(
+                Member.builder().email("test@test.com").build()
+        );
+
+        recruitmentJob = recruitmentJobRepository.save(
+                RecruitmentJob.builder().build()
+        );
+    }
 
     @DisplayName("member id로 지원한 공고 확인")
     @Test
@@ -25,5 +48,27 @@ class ApplicationInfoRepositoryTest {
         for (ApplicationInfo applicationInfo : byMemberId) {
             System.out.println(applicationInfo);
         }
+    }
+
+    @DisplayName("회원이 해당 모집 직무에 지원한 이력이 없으면 지원 결과가 빈값이다.")
+    @Test
+    void findApplyResultEmpty() {
+        Optional<ApplicationResult> applyResult = applicationInfoRepository.findApplyResult(member, recruitmentJob);
+        assertThat(applyResult).isEmpty();
+    }
+
+    @DisplayName("회원이 해당 모집 직무에 지원한 이력이 있으면 지원 결과가 반환된다.")
+    @Test
+    void findApplyResult() {
+        // given
+        ApplicationInfo applicationInfo = ApplicationInfo.of(member, recruitmentJob);
+        applicationInfoRepository.save(applicationInfo);
+
+        // when
+        Optional<ApplicationResult> applyResult = applicationInfoRepository.findApplyResult(member, recruitmentJob);
+
+        // then
+        assertThat(applyResult).isPresent();
+        assertThat(applyResult).contains(ApplicationResult.APPLY);
     }
 }
