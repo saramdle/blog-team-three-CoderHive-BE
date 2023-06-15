ffa162defc9cc762b106d40c1415eb35ba513090
anneu <b.imnuj@gmail.com>
2023-06-15 15:51:27 +0900

    Feat: 게시글 상세 조회 기능 구현

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/response/MemberResponseDto.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/dto/response/MemberResponseDto.java
@@ -1,10 +1,14 @@
 package net.blogteamthreecoderhivebe.domain.member.dto.response;
 
+import lombok.Builder;
+import net.blogteamthreecoderhivebe.domain.member.entity.Member;
+
 public class MemberResponseDto {
 
     /**
      * 게시글 상세페이지에 보여줄 회원 정보
      */
+    @Builder
     public record MemberInfoOnPostDetail(
             Long memberId,
             String profileImgUrl,
@@ -12,5 +16,14 @@ public class MemberResponseDto {
             String job,
             String level
     ) {
+        public static MemberInfoOnPostDetail from(Member member) {
+            return MemberInfoOnPostDetail.builder()
+                    .memberId(member.getId())
+                    .profileImgUrl(member.getProfileImageUrl())
+                    .nickname(member.getNickname())
+                    .job(member.getJob().getDetail())
+                    .level(member.getLevel().getDescription())
+                    .build();
+        }
     }
 }
