ae2fb1b07a0befef130a346087aa1a5122e14fb3
anneu <b.imnuj@gmail.com>
2023-06-15 15:46:45 +0900

    Feat: 합격 회원 목록을 조회하는 기능 구현
    - 게시글 상세페이지에 참여자 정보를 보여주기 위해 합격 회원 목록을 조회하는 기능 구현

--- src/main/java/net/blogteamthreecoderhivebe/domain/member/repository/querydsl/ApplyInfoCustom.java
+++ src/main/java/net/blogteamthreecoderhivebe/domain/member/repository/querydsl/ApplyInfoCustom.java
@@ -2,10 +2,14 @@ package net.blogteamthreecoderhivebe.domain.member.repository.querydsl;
 
 import net.blogteamthreecoderhivebe.domain.member.constant.ApplicationResult;
 import net.blogteamthreecoderhivebe.domain.member.entity.Member;
+import net.blogteamthreecoderhivebe.domain.post.entity.Post;
 import net.blogteamthreecoderhivebe.domain.post.entity.RecruitmentJob;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ApplyInfoCustom {
     Optional<ApplicationResult> findApplyResult(Member member, RecruitmentJob recruitmentJob);
+
+    List<Member> findPassMembers(Post post);
 }
