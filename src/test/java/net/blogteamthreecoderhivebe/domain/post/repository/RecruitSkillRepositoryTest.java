fdf5f76fd3bb79eb6997899e166bcb70e4c4f731
anneu <b.imnuj@gmail.com>
2023-06-16 15:04:35 +0900

    Test: 게시글 id로 해당 게시글의 모집 기술 목록을 조회하는 테스트 코드 개선

--- src/test/java/net/blogteamthreecoderhivebe/domain/post/repository/RecruitSkillRepositoryTest.java
+++ src/test/java/net/blogteamthreecoderhivebe/domain/post/repository/RecruitSkillRepositoryTest.java
@@ -2,9 +2,9 @@ package net.blogteamthreecoderhivebe.domain.post.repository;
 
 import jakarta.transaction.Transactional;
 import net.blogteamthreecoderhivebe.domain.info.entity.Skill;
-import net.blogteamthreecoderhivebe.domain.info.service.SkillService;
+import net.blogteamthreecoderhivebe.domain.info.repository.SkillRepository;
 import net.blogteamthreecoderhivebe.domain.post.entity.Post;
-import net.blogteamthreecoderhivebe.domain.post.service.RecruitSkillService;
+import net.blogteamthreecoderhivebe.domain.post.entity.RecruitSkill;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,22 +21,25 @@ class RecruitSkillRepositoryTest {
     @Autowired RecruitSkillRepository recruitSkillRepository;
 
     @Autowired PostRepository postRepository;
-    @Autowired SkillService skillService;
-    @Autowired RecruitSkillService recruitSkillService;
+    @Autowired SkillRepository skillRepository;
 
     @DisplayName("게시글 id로 해당 게시글에서 모집하는 기술 목록을 조회")
     @Test
     void findSkillsEqPostId() {
         // given
-        List<Long> skillIds = List.of(1L, 2L);
-        Post post = postRepository.save(Post.builder().build());
-        recruitSkillService.save(skillIds, post);
+        Skill skill1 = skillRepository.findById(1L).orElseThrow();
+        Skill skill2 = skillRepository.findById(2L).orElseThrow();
+
+        Post post = Post.builder().build();
+        post.addRecruitSkill(RecruitSkill.from(skill1));
+        post.addRecruitSkill(RecruitSkill.from(skill2));
+        postRepository.save(post);
 
         // when
         List<Skill> findSkills = recruitSkillRepository.findSkills(post.getId());
 
         // then
-        assertThat(findSkills.get(0)).isEqualTo(skillService.findOne(skillIds.get(0)));
-        assertThat(findSkills.get(1)).isEqualTo(skillService.findOne(skillIds.get(1)));
+        assertThat(findSkills).hasSize(2);
+        assertThat(findSkills).containsExactly(skill1, skill2);
     }
 }
